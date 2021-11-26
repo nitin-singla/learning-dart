29a025375bbf0a764dc1dd577f2e0de72e0ca8ae
nitin-singla <singlanitin50@gmail.com>
2021-11-26 20:11:52 +0530

    Linter Rule followed: unnecessary_brace_in_string_interps - Avoid using braces in interpolation when not needed.

--- hello_dart_extended.dart
+++ hello_dart_extended.dart
@@ -6,7 +6,10 @@ void main(List<String> args) {
 }
 
 void helloDart(String name) {
-  print('Hello, $name!');
+  print('Hello, ${name}!');
 }
 
 // String interpolation in Dart uses the ${} syntax for expressions, or just a $ for single value.
+
+// AVOID using curly braces in interpolation when not needed.
+// https://dart.dev/guides/language/effective-dart/usage#avoid-using-curly-braces-in-interpolation-when-not-needed
