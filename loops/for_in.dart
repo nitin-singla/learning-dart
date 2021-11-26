1f772664738713cf0e06044f968100f84bfa9ed5
nitin-singla <singlanitin50@gmail.com>
2021-11-26 20:52:58 +0530

    added: for_in.dart

---
+++ loops/for_in.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  List<String> planets = [
+    'Mercury',
+    'Venus',
+    'Earth',
+    'Mars',
+    'Jupiter',
+    'Saturn',
+    'Uranus',
+    'Neptune'
+  ];
+
+  for (var planet in planets) {
+    print(planet);
+  }
+}
