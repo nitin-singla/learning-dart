944d5ab0d5a8aeab237b1d589bfe50e1122be6fc
nitin-singla <singlanitin50@gmail.com>
2021-11-26 20:57:07 +0530

    added: forEach.dart

---
+++ loops/forEach.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  List<String> planets = [
+    'Mercury',
+    'Venus',
+    'Earth',
+    'Mars',
+    'Jupiter',
+    'Saturn',
+    'Uranus',
+    'Neptune'
+  ];
+
+  planets.forEach((planet) {
+    print(planet);
+  });
+}
