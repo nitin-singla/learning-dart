7625f7fa77a3b5da48024ba5077007c69a1dad15
nitin-singla <singlanitin50@gmail.com>
2021-11-27 11:15:34 +0530

    added: while.dart

---
+++ loops/while.dart
@@ -0,0 +1,11 @@
+void main() {
+  bool someConditionisTrue = true;
+
+  while (someConditionisTrue) {
+    // do some stuff
+    print('inside the while loop');
+    someConditionisTrue = false;
+  }
+
+  print('out of the while loop');
+}
